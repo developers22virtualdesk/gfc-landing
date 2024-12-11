a8c78484c074e8da63f7131bc9692745996eea39
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-12-10 23:01:14 -0600

    some final touches for deployment

---
+++ src/app/_core/constants/home.ts
@@ -0,0 +1,24 @@
+import { Icard } from "../models/generic.model";
+
+export const JOURNEY_START_POINTS: Icard[] = [
+    {
+      title: "Financial Education",
+      description: "Access valuable financial education resources to enhance your knowledge and decision-making.",
+      image: "book", // Icono de Lucide para representación visual de educación financiera
+    },
+    {
+      title: "Expert Guidance",
+      description: "Access valuable financial education resources to enhance your knowledge and decision-making.",
+      image: "user-check", // Icono de Lucide para guías de expertos
+    },
+    {
+      title: "Diverse Financial Products and Solutions",
+      description: "Access valuable financial education resources to enhance your knowledge and decision-making.",
+      image: "dollar-sign", // Icono de Lucide para productos financieros
+    },
+    {
+      title: "Customized Strategies",
+      description: "Access valuable financial education resources to enhance your knowledge and decision-making.",
+      image: "sliders", // Icono de Lucide para estrategias personalizadas
+    }
+  ];
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -7,5 +7,5 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())]
+  providers: [provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration: 'top'})), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())]
 };
